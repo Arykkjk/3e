0dd38b8f1bb238b662f2d4260cfb6584735a016b
Arykkjk <aryella.batschauer@escola.pr.gov.br>
2025-02-10 09:58:46 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+#Arrays (we'll use the numpy library)
+import numpy as np
+
+# Create an array
+array = np.array(1, 2, 3, 4, 5)
+print("Array:, array")
+
+# Accesssing elements
+print("First element:", array[0])
+print("Last element:", array[-1])
+      
+# Slicing
+print("Elements from 1 to 3:", array[1:3]) 
+
+# Slicing
+my_list = [1, 2, 3, 4, 5, ]
+print("Liat:", my_list)
+
+# Append an element
+my_list.append(6)
+
